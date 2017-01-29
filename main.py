bc906e76122ca8abac490a60c87990f06fd008da
Afif Zafri <afzafri@gmail.com>
2017-01-29 13:14:14 +0800

    get the textarea field, and send the tracking number

--- main.py
+++ main.py
@@ -6,3 +6,8 @@ path_to_chromedriver = 'C:\driver\chromedriver.exe' # Path to the chromedriver. 
 browser = webdriver.Chrome(executable_path = path_to_chromedriver)
 url = 'http://poslaju.com.my/track-trace-v2/' # poslaju tracking website URL
 browser.get(url) # open the browser and load the website
+
+# find the textarea html element using its ID
+textarea = browser.find_element_by_id('trackingNo03')
+textarea.clear() # clear the form field
+textarea.send_keys(trackNo) # send the tracking number to the form
