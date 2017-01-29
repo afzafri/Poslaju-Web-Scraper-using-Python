505b09e30271338c2a2db88f38e9dba7bc8699db
Afif Zafri <afzafri@gmail.com>
2017-01-29 13:44:43 +0800

    Receive tracking number through command line argument

--- main.py
+++ main.py
@@ -1,21 +1,36 @@
+import sys
 from selenium import webdriver
 
-trackNo = "YOUR_TRACKING_NUMBER"
+print "------------------------------------------\n"
+print "Poslaju Web Scraper by Afif Zafri \nGithub: afzafri \n"
+print "------------------------------------------\n"
 
-path_to_chromedriver = 'C:\driver\chromedriver.exe' # Path to the chromedriver. change path as needed.
-browser = webdriver.Chrome(executable_path = path_to_chromedriver)
-url = 'http://poslaju.com.my/track-trace-v2/' # poslaju tracking website URL
-browser.get(url) # open the browser and load the website
+if len(sys.argv) < 2:
 
-# find the textarea html element using its ID
-textarea = browser.find_element_by_id('trackingNo03')
-textarea.clear() # clear the form field
-textarea.send_keys(trackNo) # send the tracking number to the form
+	print "Usage: python main.py <tracking_number>"
 
-browser.find_element_by_id('bttest').click() # find the submit button, and click it
+else:
 
-# find the result table
-restable = browser.find_element_by_id('tbDetails')
+	print "Executing script...\n"
 
-# print the html table
-print restable.text
+	trackNo = sys.argv[1] # your tracking number. received via command line argument
+
+	path_to_chromedriver = 'C:\driver\chromedriver.exe' # Path to the chromedriver. change path as needed.
+	browser = webdriver.Chrome(executable_path = path_to_chromedriver)
+	url = 'http://poslaju.com.my/track-trace-v2/' # poslaju tracking website URL
+	browser.get(url) # open the browser and load the website
+
+	# find the textarea html element using its ID
+	textarea = browser.find_element_by_id('trackingNo03')
+	textarea.clear() # clear the form field
+	textarea.send_keys(trackNo) # send the tracking number to the form
+
+	browser.find_element_by_id('bttest').click() # find the submit button, and click it
+
+	# find the result table
+	restable = browser.find_element_by_id('tbDetails')
+
+	print "Results:\n"
+
+	# print the html table
+	print restable.text
